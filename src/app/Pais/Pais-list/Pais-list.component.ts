0b67d976beb5838d2e5ab350988c4ee9791b060b
Daniel Aguilera <d.aguilera@uniandes.edu.co>
2022-04-30 18:07:43 -0500

    Update Pais y creacion DTO

---
+++ src/app/Pais/Pais-list/Pais-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Pais-list',
+  templateUrl: './Pais-list.component.html',
+  styleUrls: ['./Pais-list.component.css']
+})
+export class PaisListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/Pais/Pais.ts
@@ -0,0 +1,16 @@
+export class Pais {
+  id: number;
+  nombre: string;
+  imgBandera: string;
+
+  constructor(
+    id: number,
+    nombre: string,
+    imgBandera: string,
+  ) {
+    this.id = id;
+    this.nombre = nombre;
+    this.imgBandera = imgBandera;
+  }
+}
+
