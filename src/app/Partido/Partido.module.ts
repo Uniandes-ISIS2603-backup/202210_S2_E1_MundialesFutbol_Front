2ef8035565d5755951be5f896a259cd956305217
Juan Martin Santos Ayala <78159223+JMarts32@users.noreply.github.com>
2022-05-05 14:38:57 -0500

    Update partidos

--- src/app/Partido/Partido.module.ts
+++ src/app/Partido/Partido.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PartidoListComponent } from './Partido-list/Partido-list.component';
 
 @NgModule({
   imports: [
     CommonModule
-  ]
+  ],
+  declarations: [PartidoListComponent],
+  exports : [PartidoListComponent]
 })
 export class PartidoModule { }
