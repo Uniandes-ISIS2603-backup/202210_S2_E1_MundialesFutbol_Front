0bcb5ceb49a237d3e82b4e8a3ca9d10e76eb8ead
Daniel Aguilera <d.aguilera@uniandes.edu.co>
2022-05-18 11:08:28 -0500

    Inicio y routings de arbitro y partido hechos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArbitroListComponent } from './Arbitro/Arbitro-list/Arbitro-list.component';
+import { PartidoListComponent } from './Partido/Partido-list/Partido-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ArbitroListComponent },
+  { path: '', component: PartidoListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
