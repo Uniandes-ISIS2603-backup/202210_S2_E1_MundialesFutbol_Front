cf8ca40042696cf23db3629d5237388a14d781d9
Jd-briceno <jd.briceno@uniandes.edu.co>
2022-05-18 21:28:14 -0500

    app routing entrenador

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { ArbitroListComponent } from './Arbitro/Arbitro-list/Arbitro-list.compon
 import { GolesListComponent } from './Goles/Goles-list/Goles-list.component';
 import { PartidoListComponent } from './Partido/Partido-list/Partido-list.component';
 import { JugadorListComponent } from './Jugador/Jugadores-list/Jugadores-list.component';
+import { EntrenadorListComponent } from './Entrenador/Entrenador-list/Entrenador-list.component';
 
 const routes: Routes = [
   { path: '', component: ArbitroListComponent },
   { path: '', component: PartidoListComponent },
   { path: '', component: JugadorListComponent },
-  { path: '', component: GolesListComponent }
+  { path: '', component: GolesListComponent },
+  { path: '', component: EntrenadorListComponent }
 ];
 
 @NgModule({
