55651dc8edb146e811a2ca3679827f037c324e20
Daniel Aguilera <d.aguilera@uniandes.edu.co>
2022-06-05 20:38:31 -0500

    Ultimos tests

--- src/app/Goles/Goles.service.spec.ts
+++ src/app/Goles/Goles.service.spec.ts
@@ -1,74 +1,23 @@
 /* tslint:disable:no-unused-variable */
 
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { TestBed, async, inject } from '@angular/core/testing';
 /*import { faker } from '@faker-js/faker';*/
 
-import { GolesListComponent } from './Goles-list/Goles-list.component';
-import { HttpClientModule } from '@angular/common/http';
-import { Jugador } from '../Jugador/Jugador';
-import { Partido } from '../Partido/Partido';
-import { Goles } from './Goles.ts';
 import { GolesService } from './Goles.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 
-describe('GolListComponent', () => {
-
-  let component: GolesListComponent;
-  let fixture: ComponentFixture<GolesListComponent>;
-  let debug: DebugElement;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientModule],
-     declarations: [ GolesListComponent],
-     providers: [GolesService]
-    })
-    .compileComponents();
-  }));
-
+describe('Service: Goles', () => {
   beforeEach(() => {
-    fixture = TestBed.createComponent(GolesListComponent);
-    component = fixture.componentInstance;
-
-    /*
-    let partido = new Partido(
-      faker.datatype.number(),
-      faker.lorem.sentence(),
-      faker.datatype.Date(),
-      faker.lorem.sentence(),
-      faker.lorem.sentence()
-    );*/
-
-    /*
-    let jugador = new Jugador(
-      faker.datatype.number(),
-      faker.lorem.sentence(),
-      faker.datatype.number(),
-      faker.datatype.number(),
-      faker.lorem.sentence(),
-      faker.datatype.Date(),
-      faker.lorem.sentence(),
-      faker.lorem.sentence(),
-      faker.lorem.sentence(),
-    );*/
-
-    /*
-    component.goles = [
-      new Goles(
-        faker.datatype.number(),
-        faker.lorem.sentence(),
-        jugador,
-        partido
-      ),
-    ];*/
-    fixture.detectChanges();
-    debug = fixture.debugElement;
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [GolesService]
+    });
   });
 
-  /*
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });*/
+
+  it('should ...', inject([GolesService], (service: GolesService) => {
+    expect(service).toBeTruthy();
+  }));
 });
+
--- src/app/Mundial/Mundial.service.spec.ts
+++ src/app/Mundial/Mundial.service.spec.ts
@@ -1,17 +1,19 @@
 /* tslint:disable:no-unused-variable */
 import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed, async, inject } from '@angular/core/testing';
 import { MundialService } from './Mundial.service';
 
 describe('Service: Mundial', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [MundialService]
     });
   });
 
-  /*
+
   it('should ...', inject([MundialService], (service: MundialService) => {
     expect(service).toBeTruthy();
-  }));*/
+  }));
 });
