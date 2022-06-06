439c61feb6085aa35b049e0a6b4ab763c9a9f771
Daniel Aguilera <d.aguilera@uniandes.edu.co>
2022-06-05 20:28:37 -0500

    Prueba pais corregida

--- src/app/Pais/Pais.service.spec.ts
+++ src/app/Pais/Pais.service.spec.ts
@@ -1,17 +1,19 @@
 /* tslint:disable:no-unused-variable */
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed, async, inject } from '@angular/core/testing';
 import { PaisService } from './Pais.service';
 
 describe('Service: Pais', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [PaisService]
     });
   });
 
 
-  /*it('should ...', inject([PaisService], (service: PaisService) => {
+  it('should ...', inject([PaisService], (service: PaisService) => {
     expect(service).toBeTruthy();
-  }));*/
+  }));
 });
